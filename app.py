1884b181498967624bbc558a938d855f579883f3
Shabnoor860 <shabnoorfatima860@gmail.com>
2026-03-03 04:54:23 +0530

    fix requirements

--- app.py
+++ app.py
@@ -778,14 +778,9 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        print('✅ Database ready!')
+if __name__ == "__main__":
     import os
-
-if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        print('✅ Database ready!')
+        print("✅ Database ready!")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
